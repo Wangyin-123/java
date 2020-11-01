7e3667309539684d881c10471ab614467b853aed
Wangyin-123 <72741297+Wangyin-123@users.noreply.github.com>
2020-11-02 00:11:09 +0800

    Add files via upload

---
+++ text.java
@@ -0,0 +1,9 @@
+package choose;
+
+public class text{
+	public static void main(String choose[]){
+		student stu1 = new student("1","张三","男","语文");
+		teacher tea1 = new teacher("1","李四","男","数学");
+	}
+}
+
